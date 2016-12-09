7960c6d2b9267ec58894f8d4f1508b4609db88fc
Daniel Byon <danielbyon@gmail.com>
2016-12-09 13:24:28 -0800

    Carthage support

---
+++ MultiSelectSegmentedControl/MultiSelectSegmentedControl.h
@@ -0,0 +1,19 @@
+//
+//  MultiSelectSegmentedControl.h
+//  MultiSelectSegmentedControl
+//
+//  Created by Daniel Byon on 12/9/16.
+//  Copyright © 2016 Yonat Sharon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MultiSelectSegmentedControl.
+FOUNDATION_EXPORT double MultiSelectSegmentedControlVersionNumber;
+
+//! Project version string for MultiSelectSegmentedControl.
+FOUNDATION_EXPORT const unsigned char MultiSelectSegmentedControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultiSelectSegmentedControl/PublicHeader.h>
+
+
